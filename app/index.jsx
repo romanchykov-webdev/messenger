0db8d6f9b7d6da872e44132feacad0e5cec7053f
faram <romanchykov.webdev@gmail.com>
2024-11-05 10:52:48 +0100

    initial commit

---
+++ app/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const Index = () => {
+  return (
+    <View >
+      <Text>Index works!</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({})
+
+export default Index;
